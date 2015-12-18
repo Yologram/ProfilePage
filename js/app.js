cd90486ae2465a8b5e1fac52b68eb6bc2a55c6d1
Yologram <metais.louis@hotmail.fr>
2015-12-18 09:40:54 +0100

    please

---
+++ js/app.js
@@ -0,0 +1,50 @@
+"use strict"
+$(document).ready(function(){
+	
+	$('body').on('click','a', function(event){
+		event.preventDefault();
+		$('.para').hide();
+		var montrer=$(this).attr('href');
+		$(montrer).show();
+	});
+
+	/*var yolo = function (champ, cible, json){
+		var boogie = champ;
+		var aziz = Mustache.to_html(boogie, json);
+		$(cible).html(aziz);
+	}*/
+
+	$.getJSON('https://s.idsympa.com/u-21.json', function(data){
+		var template = $('#template').html();
+		Mustache.parse(template);
+		var rendered = Mustache.render(template, data);
+		$('body').html(rendered);
+	});
+		/*yolo("{{first_name}}",$('span[elt="first_name"]'),data);
+		yolo("{{last_name}}",$('span[elt="last_name"]'),data);
+		yolo("{{occupation}}",$('span[elt="occupation"]'),data);
+		yolo("{{id}}",$('span[elt="id"]'),data);
+		yolo("{{contact.email}}",$('span[elt="email"]'),data);
+		yolo("{{contact.website}}",$('span[elt="website"]'),data);
+
+
+
+
+	});*/
+
+	});
+
+
+		
+
+
+
+	/*var result =$('[elt]')
+	for (var m=0; m<result.length; m++){
+		var attribut =$(result[m]).attr('elt');
+		var value = data[attribut];
+		$(result[m]).html(value);
+
+	}
+
+});,*/
